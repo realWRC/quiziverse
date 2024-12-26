24df750f855837a629028810b5800a7aec173ba0
Wongani Chulu <wonganichulu@gmail.com>
2024-12-26 15:08:28 +0200

    Added session management with flask session

--- api/config/config.py
+++ api/config/config.py
@@ -1,9 +1,21 @@
 from flask import Flask
 from os import environ
 from flask_login import LoginManager
+from flask_session import Session
+from datetime import timedelta
+from models import client
 
 app = Flask(__name__)
 app.secret_key = environ.get('SECRET')
+
+app.config['SESSION_PERMANENT'] = True
+app.config['SESSION_USE_SIGNER'] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
+app.config['SESSION_TYPE'] = 'mongodb'
+app.config['SESSION_MONGODB'] = client
+app.config['SESSION_MONGODB_DB'] = 'quizziverse'
+app.config['SESSION_MONGODB_COLLECTION'] = 'sessions'
+Session(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
