2cdd168c09db6295035ea9c666e52316dca4a560
Wongani Chulu <wonganichulu@gmail.com>
2024-12-26 14:26:45 +0200

    Added registration route to api

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from config.config import app, login_manager
-from flask import jsonify
+from flask import jsonify, request
 from models.user import User
 
 
@@ -24,9 +24,51 @@ def index():
 def register():
     """ Registration route
     """
-    return jsonify({
-        "message": "Successfully Registered"
-    })
+    data = request.json
+
+    if not data:
+        return jsonify({
+            "message": "Empty request"
+        }), 400
+
+    if not all(field in data for field in \
+            ['username', 'password', 'email','confirm_password']):
+        return jsonify({
+            "message": "All fields are required."
+        }), 400
+
+    if not all(isinstance(data[field], str) for \
+            field in ['username', 'email', 'password', 'confirm_password']):
+        return jsonify({
+            "message": "All fields are required and must be strings."
+        }), 400
+
+    username = data["username"].strip()
+    email = data["email"].strip()
+    password = data["password"].strip()
+    confirm_password = data["confirm_password"].strip()
+
+    if password != confirm_password:
+        return {
+            "message": "Passwords do not match"
+        }, 400
+
+    if User.getByUsername(username):
+        return {
+            "message": "Username already in use. Please choose another username."
+        }, 400
+
+    if User.getByEmail(email):
+        return {
+            "message": "Email already in use. Please choose another email."
+        }, 400
+
+    user = User(username=username, email=email, password=password)
+    user.save()
+
+    return {
+        "message": "Registration successful!"
+    }, 201
 
 
 @app.route("/login", methods=["POST"])
