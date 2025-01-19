6e2211d53897430a087c596810b7f528db7303ae
Wongani Chulu <wonganichulu@gmail.com>
2025-01-19 10:16:31 +0200

    Added documentation to information blueprint

--- api/blueprints/information.py
+++ api/blueprints/information.py
@@ -1,3 +1,7 @@
+"""
+Defines Blueprint for routes that presents simple information to
+the user, including: index, about and accout routes.
+"""
 
 from flask import flash, render_template, url_for, redirect
 from flask_login import current_user
@@ -11,21 +15,33 @@ info_bp = Blueprint('info', __name__)
 
 @info_bp.route("/", methods=["GET", "POST"])
 def index():
-    """ Welcome Page
+    """ 
+    Retrieves the index page where the product information
+    is displayed.
+
+    Response:
+        HTML page for the index page.
     """
     return render_template("index.html", title="QUIZIVERSE", year=year)
 
 
 @info_bp.route("/about", methods=["GET"])
 def about():
-    """ About page
+    """
+    Retrieves the about page where the infomation about the developer is
+    displayed.
+
+    Response:
+        HTML page for the about page.
     """
     return render_template("about.html", title="ABOUT", year=year)
 
 
 @info_bp.route("/account", methods=["GET", "POST"])
 def account():
-    """ Renders the users profile
+    """
+    Retrieves the users account information including all actions they
+    can do to their account.
     """
     if not current_user.is_authenticated:
         flash("You are are not logged in")
